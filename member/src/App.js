7aafcce7a659d4554889643b7102890a629b721a
chchcoco <117333258+chchcoco@users.noreply.github.com>
2023-03-27 15:37:10 +0900

    [rename] member와 admin 구현 파일을 분리

---
+++ member/src/App.js
@@ -0,0 +1,23 @@
+
+import MainLayout from './layouts/MainLayout';
+import MyPageLayout from './layouts/MyPageLayout';
+import PostBoardLayout from './layouts/PostBoardLayout';
+// import Modal from './Modal/Modal';
+
+
+import ModalContents from './Modal/ModalContents';
+// import Modal from './Modal/ModalHeader';
+import Course from './travelcourseComponents/Course';
+function App() {
+  return (
+
+    // <Modal/>
+    // <PostBoardLayout/>
+    // <MainLayout/>
+    // <MyPageLayout/>
+    <PostBoardLayout/>
+
+  );
+}
+
+export default App;
