7a4f90169f128f707252783e7b8879185a4b4d67
leebumsung <leebumsung3@naver.com>
2023-03-27 18:54:37 +0900

    [feat]dash 화면완성

--- admin/src/components/Dashbord_comps/VisitMem.js
+++ admin/src/components/Dashbord_comps/VisitMem.js
@@ -4,11 +4,11 @@ function VisitMem() {
 
     return (
         <div className={ dashStyle2.visitmem }>
-            <div className={ dashStyle2.inline }>    
-                <h4>전체 방문자 수</h4><h1>{7000}</h1>
+            <div className={ dashStyle2.visit2 }>    
+                <h4 className={ dashStyle2.visit3 }>전체 방문자 수</h4><h1>{7000}</h1>
             </div>
-            <div className={ dashStyle2.inline }>
-                <h4>오늘 방문자 수</h4><h1>{70}</h1>
+            <div className={ dashStyle2.visit2 }>
+                <h4 className={ dashStyle2.visit3 }>오늘 방문자 수</h4><h1>{70}</h1>
             </div>
         </div>
     )
