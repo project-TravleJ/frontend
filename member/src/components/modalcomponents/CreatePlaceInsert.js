b158faac4b9547b54a2acdc7954b52b9e179761e
yong <gpsyo911@gmail.com>
2023-03-28 16:47:12 +0900

    드디어 모달이 되는구만;;

--- member/src/components/modalcomponents/CreatePlaceInsert.js
+++ member/src/components/modalcomponents/CreatePlaceInsert.js
@@ -1,59 +1,63 @@
 import style from './modalcomponents.module.css';
-
+import { closeModal } from '../../features/modal/modalSlice';
+import { useDispatch } from 'react-redux';
 
 const CreatePlaceInsert = () => {
-
-    return(
-        
-        <aside className={ style.placecontrolModal }>
+    const dispatch = useDispatch();
+    return (
+            <aside className={style.modelbackdrop}>
+                <div className={style.placecontrolModal}>
                 <div>
                     <h1 align="center">장소 등록</h1>
                 </div>
-            <div className={ style.bigcontainer}>
-                <div className={ style.placemap }>
-                    맵 넣을 예정
-                </div>
-                <div className={ style.placecontent }>
-                    <div className={ style.createcontainer }>
-                        <div>
-                            검색 &nbsp; <input type='text' />
-                        </div>
-
-                        <div>
-                            <button className={ style.btnset }>선택</button>
-                            &nbsp; 등록된 장소에서 선택합니다.
-                            <hr/>
-                            <button className={ style.btnset }>새 등록</button>
-                            &nbsp; 새로운 장소를 등록합니다.
-                        </div>
-
-                            <div className= { style.placeplace }>
-                                <div>
-                                &nbsp; 이름 : &nbsp;&nbsp;&nbsp;&nbsp; <input type='text' />
-                                <br/> <br/>
-                                &nbsp; 타입 : &nbsp; &nbsp;&nbsp; <input type='text' className={ style.placexytext } />
-                                </div>
-                                <div className= { style.placexy }>
-                                &nbsp; 좌표 X : &nbsp; <input type='text' className={ style.placexytext }/>
-                                    &nbsp;
-                                &nbsp;  좌표 Y : &nbsp; <input type='text' className={ style.placexytext }/>
-                            </div>
+                <div className={style.bigcontainer}>
+                    <div className={style.placemap}>
+                        맵 넣을 예정
+                    </div>
+                    <div className={style.placecontent}>
+                        <div className={style.createcontainer}>
                             <div>
-                            &nbsp; 설명 : &nbsp;&nbsp;&nbsp;&nbsp; <input type='text' />
+                                검색 &nbsp; <input type='text' />
+                            </div>
+
+                            <div>
+                                <button className={style.btnset}>선택</button>
+                                &nbsp; 등록된 장소에서 선택합니다.
+                                <br/><br/>
+                                <hr />
+                                <button className={style.btnset}>새 등록</button>
+                                &nbsp; 새로운 장소를 등록합니다.
+                            </div>
+
+                            <div className={style.placeplace}>
+                                <div>
+                                    &nbsp; 이름 : &nbsp;&nbsp; <input type='text' />
+                                    <br/> <br/> <br/>
+                                    &nbsp; 타입 : &nbsp; &nbsp; <input type='text' className={style.placexytext} />
+                                </div>
+                                <div className={style.placexy}>
+                                    &nbsp; 좌표 X : &nbsp; <input type='text' className={style.placexytext} />
+                                    &nbsp;
+                                    &nbsp;  좌표 Y : &nbsp; <input type='text' className={style.placexytext} />
+                                </div>
+                                <div>
+                                    &nbsp; 설명 : &nbsp;&nbsp;&nbsp;&nbsp; <input type='text' />
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-          
-            </div>
-            <div className={ style.buttonBox}> 
-            <button className={ style.btnset }>취소</button>
-            <button className={ style.btnset }>완료</button>
-            </div>
-            <br/>   
-                     
-        </aside>
-       
+                <div className={style.buttonBox}>
+                    <button className={style.btnset} onClick={() => {
+                        dispatch(closeModal());
+                    }}>취소</button>
+                    <button className={style.btnset} onClick={() => {
+                        dispatch(closeModal());
+                    }}>완료</button>
+                </div>
+                <br />
+                </div>
+            </aside>
 
     );
 }
--- member/src/components/modalmodal/Modal.js
+++ member/src/components/modalmodal/Modal.js
@@ -1,13 +1,13 @@
 import { useDispatch } from 'react-redux';
-import { closeModal } from './features/modal/modalSlice';
+import { closeModal } from '../../features/modal/modalSlice';
 
 const Modal = () => {
   const dispatch = useDispatch();
   return (
-    <aside className='modal-container'>
-      <div className='modal'>
+    <aside>
+      <div>
         <h4>remove all items from your shopping cart?</h4>
-        <div className='btn-container'>
+        <div>
           <button
             type='button'
             onClick={() => {
