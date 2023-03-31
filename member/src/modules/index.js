17dfeb4ad0135928b183a46657fef904b870a3d5
JaeHyeon Ryu <elqm9016@gmail.com>
2023-03-31 18:17:26 +0900

    게시글 리스트 페이징 기능 구현
    구현 완료.
    리덕스 적용 실패... 적용 중...

--- member/src/modules/index.js
+++ member/src/modules/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import postReducer from './PostModule';
+import limitReducer from './LimitModule';
 
 const rootReducer = combineReducers({
     /* 해당 폴더에 새로 생성한 하위 리덕스를 여기에 추가 */
-    postReducer
+    postReducer,
+    limitReducer
 });
 
 export default rootReducer;
