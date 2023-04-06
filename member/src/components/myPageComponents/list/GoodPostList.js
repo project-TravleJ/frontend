0308588a55fe53127d6bd326d6504068e4867f48
JaeHyeon Ryu <elqm9016@gmail.com>
2023-04-06 18:06:41 +0900

    마이페이지 페이징 기능 구현
    마이페이지 및 페이징이 필요한 페이지에 페이징 기능 구현 완료.

--- member/src/components/myPageComponents/list/GoodPostList.js
+++ member/src/components/myPageComponents/list/GoodPostList.js
@@ -1,12 +1,14 @@
-import PostHeader from '../item/PostHeader';
 import Post from './GoodPostList.module.css';
-import { useEffect } from "react";
+import PostHeader from '../item/PostHeader';
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { callGetPostsAPI } from "../../../apis/PostAPI";
 import GoodPostContent from '../item/GoodPostContent';
+import Pagination from '../../postBoardComponents/Pagination';
+import Paging from '../../postBoardComponents/Pagination';
 
 function GoodPostList() {
-    
+
     const result = useSelector(store => store.post);
     console.log('postReducer result : ', result);
 
@@ -21,15 +23,32 @@ function GoodPostList() {
         },
         []
     );
+    
+    const limit = 4;
+    const [page, setPage] = useState(1);
+    const offset = (page - 1) * limit;
 
-    return(
+    return (
         <>
-            <div className={ Post.whole }>
-                <h3 className={ Post.text }># 개추한 글 목록(찜 목록)</h3>
-                <PostHeader/>
-                { posts.map(post => <GoodPostContent key={ post.postCode } post={ post }/>) }
-            </div>
+            <div className={Post.whole}>
+                <h3 className={Post.text}># 개추한 글 목록(찜 목록)</h3>
+                <PostHeader />
+                <div className={Paging.list}>
+                    <main className={ Post.marginBottom }>
+                        {posts.slice(offset, offset + limit).map(post => <GoodPostContent key={post.code} post={post} />)}
+                    </main>
 
+                    <footer>
+                        <Pagination
+                            total={posts.length}
+                            limit={limit}
+                            page={page}
+                            setPage={setPage}
+                            blockLimit={10}
+                        />
+                    </footer>
+                </div>
+            </div>
         </>
     );
 }
--- member/src/components/myPageComponents/list/WritingList.js
+++ member/src/components/myPageComponents/list/WritingList.js
@@ -1,12 +1,14 @@
 import Post from './WritingList.module.css';
 import PostHeader from '../item/PostHeader';
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { callGetPostsAPI } from "../../../apis/PostAPI";
 import WritePostContent from '../item/WritePostContent';
+import Pagination from '../../postBoardComponents/Pagination';
+import Paging from '../../postBoardComponents/Pagination';
 
 function WritingList() {
-    
+
     const result = useSelector(store => store.post);
     console.log('postReducer result : ', result);
 
@@ -22,13 +24,31 @@ function WritingList() {
         []
     );
 
-    return(
+    const limit = 4;
+    const [page, setPage] = useState(1);
+    const offset = (page - 1) * limit;
+
+    return (
         <>
-        <div className={ Post.whole }>
-        <h3 className={ Post.text }># 작성글 목록</h3>
-            <PostHeader/>
-            { posts.map(post => <WritePostContent key={ post.postCode } post={ post }/>) }
-        </div>
+            <div className={Post.whole}>
+                <h3 className={Post.text}># 작성글 목록</h3>
+                <PostHeader />
+                <div className={Paging.list}>
+                    <main className={ Post.marginBottom }>
+                        {posts.slice(offset, offset + limit).map(post => <WritePostContent key={post.code} post={post} />)}
+                    </main>
+
+                    <footer>
+                        <Pagination
+                            total={posts.length}
+                            limit={limit}
+                            page={page}
+                            setPage={setPage}
+                            blockLimit={10}
+                        />
+                    </footer>
+                </div>
+            </div>
         </>
     );
 }
