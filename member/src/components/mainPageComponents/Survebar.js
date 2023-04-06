3de5f79a0b79fd388419bff4a495e892b62a5293
yong <gpsyo911@gmail.com>
2023-04-06 18:14:33 +0900

    feat 모달컴포넌트 수정, 검색기능, 필터기능

--- member/src/components/mainPageComponents/Survebar.js
+++ member/src/components/mainPageComponents/Survebar.js
@@ -1,5 +1,5 @@
 import srvbarstyle from './usecomponents.module.css';
-import { Link, NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { openModal9 } from '../../features/modal/modalSlice9';
 import { openModal10 } from '../../features/modal/modalSlice10';
@@ -17,7 +17,7 @@ const Survebar = () => {
                 <h4><Link onClick={() => {
                         dispatch(openModal10());
                     }}>회원가입</Link></h4>
-                <h4><NavLink to="/myPage">마이페이지</NavLink></h4>
+                <h4><a href="/myPage">마이페이지</a></h4>
             </div>
         </div>
     );
--- member/src/pages/MyPage.js
+++ member/src/pages/MyPage.js
@@ -1,7 +1,5 @@
 import style from '../components/mainPageComponents/usecomponents.module.css';
-import Header from "../components/mainPageComponents/Header";
 import Footer from '../components/mainPageComponents/Footer';
-import SurvebarLogin from '../components/mainPageComponents/SurvebarLogin';
 import MyPageContents from '../components/myPageComponents/MyPageContents';
 import WritingList from '../components/myPageComponents/list/WritingList';
 import GoodPostList from '../components/myPageComponents/list/GoodPostList';
